6dee031ec65fb8b264a787f6f74c102253660ddf
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-05-25 11:38:26 +0100

    Create navbar module

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+const navbar = document.createElement("nav");
+navbar.classList.add("navbar");
+
+const logo = document.createElement("span");
+logo.textContent = "Gourmet Buffet";
+logo.classList.add("logo");
+
+const home = document.createElement("a");
+home.textContent = "Home";
+
+const menu = document.createElement("a");
+menu.textContent = "Menu";
+
+const contacts = document.createElement("a");
+contacts.textContent = "Contacts";
+
+navbar.append(logo, home, menu, contacts);
+
+export default navbar;
