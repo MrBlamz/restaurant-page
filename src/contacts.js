d330ce75f3271c4a6d59332253205ab2cb5a7fb5
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-05-26 11:59:19 +0100

    Add contacts module

---
+++ src/contacts.js
@@ -0,0 +1,30 @@
+function createContacts() {
+  const contacts = document.createElement("section");
+  contacts.classList.add("contacts");
+
+  const phone = createContactItem("Phone", "957 345 654");
+  const address = createContactItem(
+    "Address",
+    "1542 N Cahuenga Blvd, Los Angeles, CA 90028, United States"
+  );
+  const email = createContactItem("Email", "gourmet_buffet@the_best.com");
+
+  contacts.append(phone, address, email);
+  return contacts;
+}
+
+function createContactItem(header, text) {
+  const container = document.createElement("div");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = header;
+
+  const p = document.createElement("p");
+  p.textContent = text;
+
+  container.append(h2, p);
+
+  return container;
+}
+
+export default createContacts;
--- src/page-load.js
+++ src/page-load.js
@@ -2,7 +2,8 @@ import createNavbar from "./navbar";
 import createSocialContainer from "./social";
 import createHome from "./home";
 import createMenu from "./menu";
+import createContacts from "./contacts";
 
 export function loadPage(container) {
-  container.append(createNavbar(), createMenu(), createSocialContainer());
+  container.append(createNavbar(), createContacts(), createSocialContainer());
 }
