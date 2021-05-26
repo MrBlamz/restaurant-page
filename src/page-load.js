a5c647acc094734616d34d488a611f90b8e8f114
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-05-26 10:40:55 +0100

    Add social module

--- src/page-load.js
+++ src/page-load.js
@@ -3,5 +3,5 @@ import createSocialContainer from "./social";
 import createHome from "./home";
 
 export function loadPage(container) {
-  container.append(navbar, createHome());
+  container.append(navbar, createHome(), createSocialContainer());
 }
---
+++ src/social.js
@@ -0,0 +1,35 @@
+function createSocialContainer() {
+  // Creates a container to hold social media info
+  const socialContainer = document.createElement("div");
+  socialContainer.classList.add("social");
+
+  // Creates an ul element to hold li links
+  const ul = document.createElement("ul");
+
+  function createSocialElement(src) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    img.src = src;
+
+    a.appendChild(img);
+    li.appendChild(a);
+
+    return li;
+  }
+
+  const facebook = createSocialElement("/dist/icons/facebook.png");
+  const twitter = createSocialElement("/dist/icons/twitter.png");
+  const instagram = createSocialElement("/dist/icons/instagram-logo.png");
+
+  // Adds links to ul
+  ul.append(facebook, twitter, instagram);
+
+  // Adds ul to social container
+  socialContainer.appendChild(ul);
+
+  return socialContainer;
+}
+
+export default createSocialContainer;
